cf99c4ce96de3720de9b3187b1ed63fd7ba5d648
xszxc <2112067692@qq.com>
2023-12-13 20:44:27 +0800

    与前端接口对接成功，根据用户信息生成token，使用json返回数据

---
+++ src/main/java/filter/Cros.java
@@ -0,0 +1,27 @@
+package filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/*
+允许跨域请求
+ */
+@WebFilter(filterName = "cros",urlPatterns = "/*")
+public class Cros implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletResponse res = (HttpServletResponse)response;
+        res.setHeader("Access-Control-Allow-Origin", "*"); // 允许所有源
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        chain.doFilter(request, res);
+    }
+}
---
+++ src/main/java/utils/JwtUtil.java
@@ -0,0 +1,127 @@
+package utils;
+
+import entity.Token;
+import io.jsonwebtoken.*;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
+import java.util.logging.Logger;
+
+public class JwtUtil {
+    static Logger logger = Logger.getLogger(JwtUtil.class.getName());
+    /**
+     * JWT token 签名
+     * 签名密钥长度至少32位
+     */
+    private static final String JWT_SIGN_KEY = "xszhangxiaocuo_jwt_token_json_sign_key";
+    //签名密钥
+    private static final String BASE64_SECURITY = Base64.getEncoder().encodeToString(JWT_SIGN_KEY.getBytes(StandardCharsets.UTF_8));
+
+
+    /***
+     * @Description: 创建令牌
+     * @Param:
+     * @return: com.tapque.mmo.api.util.Token
+     */
+    public static Token generateJwt(Map<String, String> user, long expire) {
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;//设置签名算法
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+        //生成签名密钥
+        byte[] apiKeySecretBytes = Base64.getDecoder().decode(BASE64_SECURITY);
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+        //添加JWT的类，设置 JWT 的头部参数，声明这个令牌是一个 JSON Web Token
+        JwtBuilder builder = Jwts.builder().setHeaderParam("typ", "JsonWebToken");
+
+        //设置参数到jwt，对每一个user中对元素调用builder.claim()方法，将多个声明添加到jwt中
+        user.forEach(builder::claim);
+
+        //Token过期时间
+        long expMillis = nowMillis + expire * 1000;
+        Date exp = new Date(expMillis);
+        //设置 JWT 的发布时间（issuedAt）、生效时间（notBefore）和过期时间（expiration），然后使用指定的签名密钥和算法进行签名
+        builder
+                .setIssuedAt(now)
+                .setNotBefore(now)
+                .setExpiration(exp)
+                .signWith(signingKey, signatureAlgorithm);
+
+        //组装Token信息
+        Token tokenInfo = new Token();
+        tokenInfo.setToken(builder.compact());
+        tokenInfo.setExpire(expire);
+        tokenInfo.setExpiration(localDateTime(exp));
+        return tokenInfo;
+    }
+
+    /**
+     * Date转换为LocalDateTime
+     *
+     * @param date 日期
+     */
+    public static LocalDateTime localDateTime(Date date) {
+        if (date == null) {
+            return LocalDateTime.now();
+        }
+        //获取时间戳
+        Instant instant = date.toInstant();
+        //获取当前系统时区
+        ZoneId zoneId = ZoneId.systemDefault();
+        return instant.atZone(zoneId).toLocalDateTime();
+    }
+
+    /***
+     * @Description: 获取Claims
+     * @Param:
+     * @return: io.jsonwebtoken.Claims
+     */
+    public static Claims getClaims(String token, long allowedClockSkewSeconds) {
+        if (token.isEmpty()) {
+            return null;
+        }
+
+        return parseJwt(token, allowedClockSkewSeconds);
+    }
+
+    /***
+     * @Description: 解析jwt
+     * @Param:
+     * @return: io.jsonwebtoken.Claims
+     */
+    public static Claims parseJwt(String jsonWebToken, long allowedClockSkewSeconds) {
+        try {
+            //JWT解析器
+            return Jwts.parser()
+                    .setSigningKey(Base64.getDecoder().decode(BASE64_SECURITY))//设置签名密钥，用于验证JWT签名
+                    .setAllowedClockSkewSeconds(allowedClockSkewSeconds)//设置允许的时钟偏差秒数，这可以用来处理因服务器时间差异导致的微小时间差异问题。
+                    .build()//构建解析器
+                    .parseClaimsJws(jsonWebToken)//解析传入的 JWT 字符串
+                    .getBody();//获取其主体
+        } catch (ExpiredJwtException ex) {
+            logger.severe("token过期"+ex);
+            //过期
+            //抛异常 让系统捕获到返回到前端
+        } catch (SignatureException ex) {
+            logger.severe("签名错误"+ ex);
+            //签名错误
+            //抛异常 让系统捕获到返回到前端
+        } catch (IllegalArgumentException ex) {
+            logger.severe("token为空"+ex);
+            //token 为空
+            //抛异常 让系统捕获到返回到前端
+        } catch (Exception e) {
+            logger.severe("解析token异常"+e);
+            //抛异常 让系统捕获到返回到前端
+        }
+        return null;
+    }
+
+}
